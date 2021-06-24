2dfd0610bcc6a41e5b062640200e2d59dab9b85b
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-06-23 23:59:07 -0700

    Applying typescript to RXJS example in wikipedia.serach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,18 @@ import { WikipediaService } from './service/wikipedia.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   pages = [];
-
-
   constructor(
     private wikipedia: WikipediaService,
  ) { }
 
   onTerm(term: string) {
-    //this.wikipedia.search(term) retrieves an observable
-    //observable is going ot star to emit some events
-    //as soon as it reteives some data from wikipedia api
-    //then we call subscribe() on it and we pass it our observer
-    //(response: any) => {this.pages = response.query.search;});
-    //this function will be called any time that the obserable emits a value
+    // this.wikipedia.search(term) retrieves an observable
+    // observable is going ot star to emit some events
+    // as soon as it reteives some data from wikipedia api
+    // then we call subscribe() on it and we pass it our observer
+    // (response: any) => {this.pages = response.query.search;});
+    // this function will be called any time that the obserable emits a value
 
     /*
     in our case we receive that value "response" we pull off one property "search",
--- src/app/service/wikipedia.service.ts
+++ src/app/service/wikipedia.service.ts
@@ -2,11 +2,22 @@ import { Injectable } from '@angular/core';
 // allow the http request
 import { HttpClient } from '@angular/common/http';
 
+import { Observable } from 'rxjs';
+
+// // if we remove <number>, the "value" on line 13 will be unknown
+// const observable = new Observable <number> ((observer) => {
+//   observer.next(1);
+// });
+
+// // hover over "value", ==> number
+// observable.subscribe((value) => {
+//   console.log(value);
+// });
+
 @Injectable({
   providedIn: 'root'
 })
 export class WikipediaService {
-
   constructor(
     // dependency injection
     private http:HttpClient,
@@ -15,8 +26,20 @@ export class WikipediaService {
 
   public search(term: string) {
     // to make a request
-    //returns an observable
+    // returns an observable
     return this.http.get('https://en.wikipedia.org/w/api.php', {
+      /**
+          tslint:disable-next-line: jsdoc-format
+          tslint:disable-next-line: jsdoc-format
+          we passed some arguments to customize network request
+          tslint:disable-next-line: jsdoc-format
+          tslint:disable-next-line: jsdoc-format
+          it tuurns out that http.net() returns an observable
+          as soon as a request is completed,
+          the observeable is going to emit a valud consisting tof all data
+          tslint:disable-next-line: jsdoc-format
+          that are retreived from the api https://en.wikipedia.org/w/api.php
+       */
       params: {
         action: 'query',
         format: 'json',
